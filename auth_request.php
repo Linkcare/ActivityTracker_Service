18c93ac0fdbc8de81f77802ca8fc4214d298225d
Rubèn Ostos <ruben.ostos@linkcareapp.com>
2021-09-21 11:30:38 +0200

    LC2-8691 Activity Tracking Service: OAuth authentication should request a login always

--- auth_request.php
+++ auth_request.php
@@ -6,7 +6,8 @@ use FitbitOAuth2Client\Fitbit;
 if (isset($_GET['task'])) {
     $authorizationUrl = Fitbit::getProvider()->getAuthorizationUrl(['state' => $_GET['task'], 'scope' => ['activity', 'settings']]);
 
-    header('Location: ' . $authorizationUrl);
+    // The prompt = 'login' parameter will force at the Fitbit side to log in
+    header('Location: ' . $authorizationUrl . '&prompt=login');
     exit();
 }
 ?>
